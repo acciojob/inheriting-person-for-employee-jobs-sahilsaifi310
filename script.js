46aa2ced7a068d087e010195ef62c0e308e6cfdb
sahilsaifi310 <shahilsaifi750@gmail.com>
2025-02-17 15:05:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,32 +1,27 @@
-
 class Person {
-    
-    cangreet(name, age) {
+    constructor(name, age) {
         this.name = name;
         this.age = age; 
+    }
+
+    greet() {
         console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
     }
 }
 
-
 class Employee extends Person {
     constructor(name, age, jobTitle) {
-       
         super(name, age); 
         this.jobTitle = jobTitle;
     }
 
-   
     jobGreet() {
-        super.cangreet()
         console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
     }
 }
 
-
 const person = new Person("Alice", 25);
-person.cangreet(); 
-
+person.greet(); 
 
 const employee = new Employee("Bob", 30, "Manager");
 employee.jobGreet(); 
