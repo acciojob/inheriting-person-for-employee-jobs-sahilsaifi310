10cd2da05b02f1cc8a4fc4b1bf0a4f20bac19794
sahilsaifi310 <shahilsaifi750@gmail.com>
2025-02-17 14:27:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
-// complete this js code
-function Person(name, age) {}
+class Person {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
 
-function Employee(name, age, jobTitle) {}
+    greet() {
+        console.log(`Hello, my name is ${this.name}, I am ${this.age} years old`);
+    }
+}
 
-// Do not change code below this line
-window.Person = Person;
-window.Employee = Employee;
+class Employee extends Person {
+    constructor(name, age, jobTitle) {
+        super(name, age); 
+        this.jobTitle = jobTitle;
+    }
+
+    greet() {
+        super.greet(); 
+        console.log(`My job title is ${this.jobTitle}`);
+    }
+}
+
+
+let obj2 = new Employee("Bob", 30, "Manager");
+obj2.greet();
+
